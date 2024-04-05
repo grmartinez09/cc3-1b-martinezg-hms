8c1ed1cd4054a84d09d010c46df024f96877f6db
Gerryle R. Martinez <grm7867@students.uc-bcf.edu.ph>
2024-04-05 11:35:26 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace cc3_1b_martinezg_hms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
